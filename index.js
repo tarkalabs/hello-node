e22eadcf93e48e968239b6a5fa3a0e6b80da932b
Vagmi Mudumbai <vagmi.mudumbai@gmail.com>
2016-02-02 11:28:46 +0530

    hello node

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict"
+const reddit = require('./lib/reddit');
+
+//var models = require('./lib/models');
+//models.hello();
+//models();
+
+//models.on('event', function() {
+  //console.log('received event');
+  //console.log(arguments);
+//});
+
+//console.log('emitting event...');
+//models.emit('event', 1 ,2,3);
+
+reddit.dumpReddit("reddit_content.json",(err) => {
+  console.log("done");
+});

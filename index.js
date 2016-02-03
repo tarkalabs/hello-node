1feaf496952288378c6486ace9796200701b139b
Vagmi Mudumbai <vagmi.mudumbai@gmail.com>
2016-02-03 09:57:01 +0530

    WIP - login page added auth in progress

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict"
 const express = require("express");
 const session = require("express-session");
+const bodyParser = require("body-parser");
 const map = require("lodash/map");
 const pick = require("lodash/pick");
 const reddit = require("./lib/reddit");
@@ -15,6 +16,7 @@ app.use(session({
   resave: true,
   saveUninitialized: true
 }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/",(req, res) => {
   if(req.session.views) {
@@ -25,21 +27,36 @@ app.get("/",(req, res) => {
   reddit.readReddit(REDDIT_FILE,(err,data) => {
     if(err) {
       res.status(500);
-      res.json(JSON.stringify(err));
+      res.json(err);
       return;
     }
     let content = JSON.parse(data);
     let entries = map(content.data.children, (child)=> pick(child.data, ["title","score","num_comments","url"]))
     console.log(entries);
-    res.render("index", {entries: entries, viewCount: req.session.views});
+    res.render("index", {
+      currentUser: req.session.currentUser,
+      entries: entries,
+      viewCount: req.session.views});
   });
+});
+
+app.get("/login", (_, res) => res.render("login") )
+app.post("/login", (req, res) => {
+  let username = req.body.login;
+  let password = req.body.password;
+  if(username!=="admin" || password!=="password") {
+    res.render("login",{error: "Username or password incorrect"});
+  } else {
+    req.session.currentUser = "admin"
+    res.redirect("/");
+  }
 });
 
 app.post("/refresh", (req,res) => {
   reddit.dumpReddit(REDDIT_FILE,(err) => {
     if(err) {
       res.status(500);
-      res.json(JSON.stringify(err));
+      res.json(err);
       return;
     }
     res.json({message: "content refreshed"});
